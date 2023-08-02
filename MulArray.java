8d6f1b6fafa477ebff21a0b9acb30b31bb4234b6
Dhanesh6864 <112810784+Dhanesh6864@users.noreply.github.com>
2023-08-02 06:22:31 +0530

    Add files via upload

---
+++ MulArray.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class MulArray {
+public static void main(String[] args)
+{
+int[][] arr = { { 7, 9 }, { 6, 8 } }; 
+for (int i = 0; i < 2; i++) 
+{
+ for (int j = 0; j < 2; j++) 
+ {
+  System.out.println(arr[i][j]+ " ");
+ }
+ //System.out.println(" ");
+}
+}
+}
